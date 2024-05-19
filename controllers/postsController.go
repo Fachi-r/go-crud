b3aef98ee70972130c4a8550cb0b751537d19b7a
Fachi-r <farairubvuta@gmail.com>
2024-05-19 22:25:45 +0200

    added more routes

--- controllers/postsController.go
+++ controllers/postsController.go
@@ -28,3 +28,62 @@ func CreatePosts(c *gin.Context) {
 		"post": post,
 	})
 }
+
+func GetPosts(c *gin.Context) {
+	// Get Posts
+	var posts []models.Post
+	initializers.DB.Find(&posts)
+
+	// Respond with Posts
+	c.JSON(200, gin.H{
+		"posts": posts,
+	})
+}
+
+func GetPost(c *gin.Context) {
+	// Get ID from url
+	id := c.Param("id")
+	// Get Posts
+	var post models.Post
+	initializers.DB.Find(&post, id)
+
+	// Respond with Posts
+	c.JSON(200, gin.H{
+		"post": post,
+	})
+}
+
+func UpdatePost(c *gin.Context) {
+	// Get ID from url
+	id := c.Param("id")
+	// get post with ID
+	var post models.Post
+	initializers.DB.Find(&post, id)
+
+	// Get Data from request body
+	var body struct {
+		Title string
+		Body  string
+	}
+
+	c.Bind(&body)
+
+	// update post
+	initializers.DB.Model(&post).Updates(models.Post{Title: body.Title, Body: body.Body})
+
+	// Respond with Posts
+	c.JSON(200, gin.H{
+		"post": post,
+	})
+}
+
+func DeletePost(c *gin.Context) {
+	// Get ID off URL
+	id := c.Param("id")
+
+	// Delete Post
+	initializers.DB.Delete(&models.Post{}, id)
+
+	// Respond with status
+	c.Status(200)
+}
